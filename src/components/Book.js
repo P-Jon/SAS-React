c4c3c3430f33d83d68b80fbea2b97284bb4ee27e
Jonathon Cross <j.cross3@lancaster.ac.uk>
2022-03-14 14:15:37 +0000

    Initial Book template

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,10 +2,31 @@ import React, { Component } from "react";
 
 import './styles/book.css';
 
-export default function BookPlaceholder() {
+export default function BookPage(props) {
     return (
-        <div className="BookPlaceholder">
+        <div className="App" style={{ 'justifyContent': 'left' }}>
+            <div className="BookRegion">
+                <h1> Hello </h1>
+                <div className="BackArrow">
 
-        </div>
+                </div>
+
+                <div className="BookPage">
+                    <div className="PageImage">
+                        <img className="BookCover" src={require("../books/" + props.imgUrl + "/img/cover.png")} />
+
+                    </div>
+
+                    <div className="PageDescription">
+
+                    </div>
+                </div>
+
+                <div className="FowardArrow">
+                </div>
+
+
+            </div>
+        </div >
     );
 }
